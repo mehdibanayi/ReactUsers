98e1dc3cb3118f9d5c734d09b5290da2bea128bd
mehdibanayi <meb.kh61@gmail.com>
2022-06-12 13:07:36 +0430

    delet toggle edit in redux

---
+++ src/components/todos/todoItem.js
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux'
+import {deleteTodo , toggleDoneTodo , editTodo} from './../../store/slices/todosSlice'
+
+export default function TodoItem({todo})
+{
+    const dispatch=useDispatch()
+    const todoDeleteHandler = () => dispatch(deleteTodo(todo.id))
+    const toggleDoneTodoHandler = () => {
+        dispatch(toggleDoneTodo({id:todo.id}))
+        dispatch(editTodo({id:todo.id,text:'new text'}))
+    }
+    return(
+    <div className="flex mb-4 items-center">
+        <p className={`mr-auto ${todo.done ? 'line-through text-green-600' : 'text-gray-700'}`}>{todo.text}</p>
+        {
+            todo.done 
+            ? <button onClick={toggleDoneTodoHandler} className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-400 border-gray-400 hover:bg-gray-400">Not Done</button>
+            : <button onClick={toggleDoneTodoHandler} className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600">Done</button>
+        }
+        <button onClick={todoDeleteHandler} className="p-1 px-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600">Remove</button>
+    </div>
+    )
+}
--- src/store/slices/todosSlice.js
+++ src/store/slices/todosSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
 const todosSlice = createSlice({
     name : 'todos',
     initialState : {
@@ -9,11 +8,34 @@ const todosSlice = createSlice({
     reducers : {
         addTodo : (state , action ) => {
             state.list.push(action.payload)
-        }
+        },
+        deleteTodo : (state , action ) => {
+            state.list=state.list.filter(todo => todo.id !== action.payload )
+        },
+        toggleDoneTodo : (state , {payload} ) => {
+            state.list=state.list.map((todo)=>{
+                return todo.id===payload.id
+                ?{
+                    ...todo,
+                    done:!todo.done
+                }
+                : todo
+            })
+        }, 
+        editTodo : (state , {payload} ) => {
+            state.list=state.list.map((todo)=>{
+                return todo.id===payload.id
+                ?{
+                    ...todo,
+                    text: payload.text
+                }
+                : todo
+            })
+        }   
     }
 })
 
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo,deleteTodo,toggleDoneTodo,editTodo } = todosSlice.actions;
 
 export default todosSlice.reducer;
